2bc70da2395d8bf49a1707f4226e2a7d771e68cd
Andrea Thomas <ANDREA.THOMAS19460@GMAIL.COM>
2020-01-16 21:44:41 -0800

    search is functioning(ish)

--- search-files/searchScript.js
+++ search-files/searchScript.js
@@ -1,62 +1,34 @@
+var searchBtn = $('#searchBtn');
+searchBtn.on('click', search);
 
-// need plot, image, title, "character,"" publisher
+function search(){
+  var searchTerm = $('#search-term').val().trim().toLowerCase();
+  var dropDown = $('#drop-down').val().trim().toLowerCase();
+  var queryURL = 'https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/search/?format=json&sort=name:asc&api_key=7e2d88929b01cf8dd519b8f457eee445ba0013d6&query=' + searchTerm;
+  var filter;
 
-
-// for character: image, title of comic/series they're in, author, character history and name
-
-// for title: image, author, title of comic/series, plot
-
-// for publisher: image, title of comic/series, name of author, plot
-
-// forEach and createElement. Iterate through the response, creating a new element for each friend and appending it to the body.
-
-// result.friendslist.friends.forEach(function(friend){
-//     var friendDiv = document.createElement('div');
-//     friendDiv.appendChild(document.createTextNode(friend.steamid));
-//     document.body.appendChild(friendDiv);
-// });
-// NB: You could make this more performant by doing only a single append to the body if you created a wrapper element and filled that with the friends before appending it. Like this:
-
-// var wrapperDiv = document.createElement('div');
-// result.friendslist.friends.forEach(function(friend){
-//     var friendDiv = document.createElement('div');
-//     friendDiv.appendChild(document.createTextNode(friend.steamid));
-//     wrapperDiv.appendChild(friendDiv);
-// });
-// document.body.appendChild(wrapperDiv);
+  $.ajax(queryURL, {
+    method: 'GET'
+  }).then(function(response){
+    console.log(response);
+    switch(dropDown) {
+      case 'character':
+        filter = response.results.name;
+        break;
+      case 'publisher':
+        filter = respone.publisher.name;
+        break;
+      case 'title':
+        filter = response.title;
+        break;
+      default: 
+        console.error('drop down value of ' + dropDown + ' not legit');
+    };
+    // if(filter === searchTerm){
       
+      // create cards and append to DOM
+    // }
+  });
+};
 
-
-
-
-
-
-// // key = 7e2d88929b01cf8dd519b8f457eee445ba0013d6
-
-var title = "";
-var character = "";
-var publisher = "";
-
-var searchTerm = $('#search-term').val().trim().toLowerCase();
-
-function buildQueryURL() {
-
-    var queryURL = 'https://comicvine.gamespot.com/api/search/?format=json&sort=name:asc&api_key=';
-    
-    var queryParams = '7e2d88929b01cf8dd519b8f457eee445ba0013d6';
-    queryParams.q = $('#search-term')
-      .val()
-      .trim();
-
-//     console.log(queryURL + $.param(queryParams));
-//     return queryURL + $.param(queryParams);
-//   };
-var queryURL = buildQueryURL();
-
-// Make the AJAX request to the API - GETs the JSON data at the queryURL.
-// The data then gets passed as an argument to the updatePage function
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-}).then(updatePage);
-});
+ 
