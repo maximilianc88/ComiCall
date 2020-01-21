58cd8efaec43d7f65070eca1be661d47efc45b62
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-01-20 20:51:16 -0800

    bug fixes

--- search-files/searchScript.js
+++ search-files/searchScript.js
@@ -1,41 +1,77 @@
-var searchBtn = $('#searchBtn');
-searchBtn.on('click', search);
+var searchBtn = $("#searchBtn");
+searchBtn.on("click", search);
 
-function search(){
-  var searchTerm = $('#search-term').val().trim().toLowerCase();
-//   var dropDown = $('#drop-down').val().trim().toLowerCase();
-  var queryURL = 'https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/search/?format=json&sort=name:asc&api_key=7e2d88929b01cf8dd519b8f457eee445ba0013d6&limit=1&query=' + searchTerm;
+function search() {
+  var searchTerm = $("#search-term")
+    .val()
+    .trim()
+    .toLowerCase();
+  var dropDown = $("#drop-down")
+    .val()
+    .trim()
+    .toLowerCase();
+  var queryURL =
+    "https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/search/?format=json&sort=name:asc&api_key=7e2d88929b01cf8dd519b8f457eee445ba0013d6&limit=20&query=" +
+    searchTerm;
   var filter;
 
   $.ajax(queryURL, {
-    method: 'GET'
-  }).then(function(response){
-    console.log(response);
-    // switch(dropDown) {
-    //   case 'character':
-    //     filter = response.results[0].name;
-    //     break;
-    //   case 'publisher':
-    //     filter = response.results[0].publisher.name;
-    //     break;
-    //   case 'title':
-    //     filter = response.results[0].name;
-    //     break;
-    //   default: 
-    //     console.error('drop down value of ' + dropDown + ' not legit');
+    method: "GET"
+  }).then(function(response) {
+     switch (dropDown) {
+      case "name":
+        for (var i=0; i<response.results.length; i++){
+        filter = response.results[i].name;
+        if (filter.toLowerCase() === searchTerm) {
+          console.log(response.results[i].name);
 
-        // var imageUrl = response.results.image.medium_url;
-        // console.log(imageUrl);
+          var newCardDiv = $('<div class="comicCardForRealz paper card" class="card text-card" style="display: flex; flex-direction: row"></div>');
+          var newCardBodyDiv = $('<div class="card-body card-body-for-realz" style="display: flex; flex-direction: column; justify-content: center"></div>');
+          var newCardH4 = $('<h4 class="card-title"></h4>');
+          var newCardImg = $('<img class="cardImage"></img>');
+          var newCardP = $('<p class="card-text cardPlot"></p>');
+          var newButton = $('<button class="searchBtn">Add to Wishlist</button>');
 
-        // var comicImage = $('<img>');
-        // comicImage.attr('src', imageUrl);
-        // comicImage.attr('alt', 'comic image');
+           $(newCardBodyDiv).append(newCardH4);
+           $(newCardBodyDiv).append(newCardImg);
+           $(newCardBodyDiv).append(newCardP);
+           $(newCardBodyDiv).append(newButton);
+           $(newCardDiv).append(newCardBodyDiv);
+           $("body").append(newCardDiv);
 
-        $('#cardTitle').text(response.results[0].name);
-        $('#cardPlot').text(response.results[0].deck);
-        $('#cardImage').attr("src", response.results[0].image.original_url);
-      
-  });
-};
+           $(newCardImg).attr("src", response.results[i].image.original_url);
+           $(newCardH4).text(response.results[i].name);
+           $(newCardP).text(response.results[i].deck);
+    }
+    };
+        break;
+      case "publisher":
+        for (var i=0; i<response.results.length; i++){
+          filter = response.results[i].publisher.name;
+          if (filter.toLowerCase()  === searchTerm) {
+          console.log(response.results[i].publisher.name);
 
- 
+          var newCardDiv = $('<div class="comicCardForRealz paper card" class="card text-card" style="display: flex; flex-direction: row"></div>');
+          var newCardBodyDiv = $('<div class="card-body card-body-for-realz" style="display: flex; flex-direction: column; justify-content: center"></div>');
+          var newCardH4 = $('<h4 class="card-title"></h4>');
+          var newCardImg = $('<img class="cardImage"></img>');
+          var newCardP = $('<p class="card-text cardPlot"></p>');
+          var newButton = $('<button class="searchBtn">Add to Wishlist</button>');
+
+           $(newCardBodyDiv).append(newCardH4);
+           $(newCardBodyDiv).append(newCardImg);
+           $(newCardBodyDiv).append(newCardP);
+           $(newCardBodyDiv).append(newButton);
+           $(newCardDiv).append(newCardBodyDiv);
+           $("body").append(newCardDiv);
+
+           $(newCardImg).attr("src", response.results[i].image.original_url);
+           $(newCardH4).text(response.results[i].name);
+           $(newCardP).text(response.results[i].deck);
+      }
+      };
+        break;
+      default:
+        console.error("drop down value of " + dropDown + " not legit");
+    }
+  })}
